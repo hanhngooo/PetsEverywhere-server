881d3567ca987371e5efd8a36eeb8c0c5d2f63b4
hanhngooo <hanhngo1795@gmail.com>
2020-06-19 21:03:53 +0200

    add profile_pic to user model

--- models/user.js
+++ models/user.js
@@ -16,6 +16,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      profile_pic: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {}
   );
